d9a11962b82f5d26a5fa18dc3bcb2c25a48cb0bd
Subhash C Gupta <subhash.gupta@tavant.com>
2019-07-15 11:59:31 +0530

    SpringBootHibernate

---
+++ src/main/java/com/programmer/gate/service/SoccerService.java
@@ -0,0 +1,9 @@
+package com.programmer.gate.service;
+
+import java.util.List;
+
+public interface SoccerService {
+	public List<String> getAllTeamPlayers(long teamId);
+	public void addBarcelonaPlayer(String name, String position, int number);
+
+}
